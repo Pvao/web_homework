735156aaa0eae826c9771fd63dc578519f2102d8
Anton <Pvao>
2023-02-19 15:38:36 +0300

    homework-1

--- homework-1/src/FindInMyStorage.groovy
+++ homework-1/src/FindInMyStorage.groovy
@@ -1,3 +1,26 @@
-
 class FindInMyStorage {
+    static void main(String[] args){
+        printf "Ввод товара: "
+        newReader : def findItem = System.in.newReader().readLine()
+
+        //Read file
+        File file1 = new File('/Storage/storage.txt');
+        def lines=file1.readLines()
+        def lastline=(lines.size()-1)
+        def founded = false
+
+        for (def i=0;i<=lastline;i++)
+        {
+            def Item = lines.get(i)
+            if (Item.equals(findItem)){
+                founded = true
+                break
+            }
+        }
+        if(founded.equals(true)){
+            println "Item Found " + findItem
+        } else {
+            println "Item not found " + findItem
+        }
+    }
 }
